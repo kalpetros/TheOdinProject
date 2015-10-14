c17d96365214c6b2b9615e03050dca9c495e3188
Petros Kalogiannakis <kalpetros@gmail.com>
2015-10-14 18:50:17 +0300

    Tagging feature completed (blogger project)

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
+Blogger::Application.routes.draw do
 	root to: 'articles#index'
-	resources :articles do
-		resources :comments
+  	resources :articles do
+    	resources :comments
 	end
+  	resources :tags
 end
